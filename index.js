eb9c3437e1caa19d1e7b32f5bd7ad0249fb6a483
toure <tourekadiy@gmail.com>
2017-08-23 15:58:40 +0200

    étape 4 terminée => installation serveur +  dossier public;

---
+++ index.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const express = require('express');
+//const mustache = require('mustache');
+
+let app = express();
+
+app.use(express.static("public"));
+
+app.listen(80, function() {
+    console.log('Server listening on port 80...');
+});
